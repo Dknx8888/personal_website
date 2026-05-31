c132abdf0c290cf979cbd06b241e16939dabae96
Dknx8888 <nick.dev.8888@gmail.com>
2026-05-31 19:41:31 -0400

    Finished nearly all the contents for the projects page.

---
+++ src/data/experiences.ts
@@ -0,0 +1,23 @@
+export const experiences = [
+    {
+        company: "United Wholesale Mortgage",
+        role: "Software Developer, Contractor",
+        date: "Sep 2025 - Dec 2025",
+        location: "Pontiac, MI",
+        description: "Built the front end for an internal goals dashboard to help UWM's IT organization track performance."
+    },
+    {
+        company: "Techainer",
+        role: "Machine Learning Engineer Intern",
+        date: "May 2024 – Aug 2024",
+        location: "Hanoi, Vietnam",
+        description: "Developed and evaluated a Vietnamese text-to-speech pipeline by fine-tuning Parler-TTS and CosyVoice."
+    },
+    {
+        company: "Imagine Software Club at MSU",
+        role: "Artificial Intelligence Engineer - Client Project - YouLearn.AI",
+        date: "Sep 2023 – Dec 2023",
+        location: "East Lansing, MI",
+        description: "Built a curriculum generator based on user-selected topics and learning progress."
+    }
+]
---
+++ src/data/projects.ts
@@ -0,0 +1,17 @@
+import testronautImg from "../assets/project_pics/testronaut-menu.png"
+import jesterImg from "../assets/project_pics/jester-io-upload.png"
+
+export const projects = [
+    {
+        banner: testronautImg,
+        title: "Testronaut",
+        description: "A project for Oakland University's hackathon, Grizzhacks (we won second place! :)). A CLI tool that makes various parts of the software development lifecycle faster, including test cases generation, code performance analysis, refactoring suggestions, and CI/CD pipeline check.",
+        github: "https://github.com/Dknx8888/testronaut"
+    },
+    {
+        banner: jesterImg,
+        title: "Jester.io",
+        description: "",
+        github: "https://github.com/viraj-shahh/jester"
+    },
+]
